4ab71de5a8c661abf6f4b29e21b2b74273c85688
Dahir Mohamed <dmoabdi98@gmail.com>
2022-02-02 13:50:51 +0000

    able to return the rates

--- src/resolvers/currencies.js
+++ src/resolvers/currencies.js
@@ -1,10 +1,9 @@
-const currencies = (parent, args, info, context) => {
+const currencies = (parent, args) =>
   parent?._currencies
     ? Object.entries(parent?._currencies).map(([code, value]) => ({
         ...value,
         code,
       }))
     : [];
-};
 
 module.exports = currencies;
--- src/resolvers/rates.js
+++ src/resolvers/rates.js
@@ -1,3 +1,13 @@
-const rates = () => {};
+const axios = require("axios");
+
+const rates = async () => {
+  const url = `https://openexchangerates.org/api/latest.json?app_id=01ff3023a68848a08c4e98a88c54629c`;
+
+  const { data } = await axios.get(url);
+
+  if (data?.rates) {
+    return Object.entries(data?.rates).map(([code, rate]) => ({ code, rate }));
+  } else return [];
+};
 
 module.exports = rates;
